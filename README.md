7fd86f4decfa67d9fad35c13e0470fba351bfdca
David Winter <i@djw.me>
2013-03-27 17:19:39 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+# puppet-postfix
+
+A simple setup of Postfix that just does outgoing email.
+
+Handy for your `php` scripts, and system services like `monit` and `cron`.
---
+++ manifests/init.pp
@@ -0,0 +1,23 @@
+class postfix {
+	package { 'postfix':
+		ensure => present,
+	}
+
+	service { 'postfix':
+		require => Package['postfix'],
+	}
+
+	exec { 'postfix set mydestination':
+		command => "sed -i.bak 's/mydestination = .*/mydestination = localhost/' /etc/postfix/main.cf",
+		unless => 'grep "mydestination = localhost" /etc/postfix/main.cf',
+		require => Package['postfix'],
+		notify => Service['postfix'],
+	}
+
+	exec { 'postfix set inet_interfaces':
+		command => "sed -i.bak 's/inet_interfaces = .*/inet_interfaces = 127.0.0.1/' /etc/postfix/main.cf",
+		unless => 'grep "inet_interfaces = 127.0.0.1" /etc/postfix/main.cf',
+		require => Package['postfix'],
+		notify => Service['postfix'],
+	}
+}
